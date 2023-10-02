0227e3593037cde38370234e49827d4f47874b4f
ahmedelselly <ahmedelselly@localhost.localdomain>
2023-10-02 09:52:15 +0300

    removed all functions to add or remove points and segments

--- js/script.js
+++ js/script.js
@@ -16,44 +16,6 @@ const s4 = new Segment(p2, p3);
 
 const graph = new Graph([p1, p2, p3, p4], [s1, s2, s3, s4]);
 
-function addRandomPoint() {
-    const success = graph.tryAddPoint(new Point(Math.random() * myCanvas.width, Math.random() * myCanvas.height));
-    ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
-    graph.draw(ctx);
-}
-
-function addRandomSegment() {
-    const index1 = Math.floor(Math.random() * graph.points.length);
-    const index2 = Math.floor(Math.random() * graph.points.length);
-    const newSegment = new Segment(graph.points[index1], graph.points[index2]);
-    const success = graph.tryAddSegment(newSegment);
-    ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
-    graph.draw(ctx);
-}
-
-function removeRandomSegment() {
-    if (graph.segments.length === 0) {
-        console.log('No segments');
-        return;
-    }
-    const index = Math.floor(Math.random() * graph.segments.length);
-    graph.removeSegment(graph.segments[index]);
-    updateCanvas();
-}
-
-function removeRandomPoint() {
-    if (graph.points.length === 0) {
-        console.log('No points');
-        return;
-    }
-    const index = Math.floor(Math.random() * graph.points.length);
-    graph.removePoint(graph.points[index]);
-    updateCanvas();
-}
-
-function removeAll() {
-    graph.dispose();
-}
 
 function updateCanvas(){
     ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
